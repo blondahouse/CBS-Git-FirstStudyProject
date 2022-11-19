23f15befb050a6bde5d6cdf5d847169c93db28cb
blondahouse <113552540+blondahouse@users.noreply.github.com>
2022-11-19 18:33:59 +0200

    Add files via upload

---
+++ CBS-PYT-L02-001-SrringsAndMath.py
@@ -0,0 +1,18 @@
+# Task 01.
+# Input string of two words divided with space.
+# Return two words divided with coma.
+
+text_input = input("Enter some string of two words divided with space: ")
+print(text_input.split())
+
+# Task 02.
+# Multiplication of three inputted integers
+a = int(input("Enter integer a: "))
+b = int(input("Enter integer b: "))
+x = int(input("Enter integer x: "))
+
+message = f'{a} * {b} * {x} = {a * b * x}'
+print(message)
+
+# Task 03.
+
